d784a1464db24a64582e499ac9905bac425778c4
Heather Callear <heather.l.c.code@gmail.com>
2026-07-11 18:56:24 +0100

    feat: print results with exponentially increasing number of runs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 use deterministic_card_game::Game;
 
+const MAX_EXPONENT: usize = 9;
+
 fn main() {
     let mut game = Game::new();
 
-    for _ in 0..10 {
-        game.play_games(1_000_000);
+    game.play_games(10);
+    let mut already_ran: usize = 10;
+    for exponent in 2..=MAX_EXPONENT {
+        game.play_games(already_ran * 9);
+        already_ran *= 10;
+        // convert results array items to "0" if 0, else scientific notation
+        let mut results_proportion_str = String::from_iter(
+            game.get_results_proportion().iter().map(
+                |f| if *f == 0f64 {format!("{f}, ")} else {format!("{:.5e}, ", f)}
+            )
+        );
+        // removing trailing commma and space
+        results_proportion_str.pop();
+        results_proportion_str.pop();
+        // print output
         println!(
-            "{} runs {:?}",
+            "10^{0} ({1:>2$}) runs: {3}",
+            exponent,
             game.total_runs,
-            game.get_results_proportion()
+            MAX_EXPONENT + 1,
+            results_proportion_str,
         );
         println!();
     }
