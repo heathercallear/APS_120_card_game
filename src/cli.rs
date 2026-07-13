8bbd97364581600ce73be51008f0a9bb8af6b5f1
Heather Callear <heather.l.c.code@gmail.com>
2026-07-13 21:44:28 +0100

    feat: improve help text for CLI arguments

--- src/cli.rs
+++ src/cli.rs
@@ -9,14 +9,16 @@ pub struct Cli {
     #[arg(default_value_t = DEFAULT_EXPONENT)]
     pub max_exponent: usize,
 
-    /// Whether to save csv file of game run data
+    /// Save csv file of game run data
     #[arg(short, long)]
     pub save_data: bool,
 
     /// Print less information (only show final results / show no result at all)
     /// 
     /// Give once for mostly quiet, and twice for completely quiet.
+    /// 
     /// When printing results, only print results after all runs, or nothing.
+    /// 
     /// When saving data, only print that data has been saved to file, or nothing.
     #[arg(short, long, action = clap::ArgAction::Count)]
     pub quiet: u8,
