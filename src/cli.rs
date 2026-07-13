a35806236e3131162a0c929cc0ab1980cdb7811f
Heather Callear <heather.l.c.code@gmail.com>
2026-07-13 21:33:48 +0100

    fix: help text not included for CLI arguments

--- src/cli.rs
+++ src/cli.rs
@@ -5,19 +5,23 @@ const DEFAULT_EXPONENT: usize = 7;
 #[derive(Parser)]
 #[command(version, about)]
 pub struct Cli {
-    // Exponent of 10 (10^?) for the number of runs to perform in total
+    /// Exponent of 10 (10^?) for the number of runs to perform in total
     #[arg(default_value_t = DEFAULT_EXPONENT)]
     pub max_exponent: usize,
 
-    // Whether to save csv file of game run data
+    /// Whether to save csv file of game run data
     #[arg(short, long)]
     pub save_data: bool,
 
-    // Print less information (only show final result / show no result at all)
+    /// Print less information (only show final results / show no result at all)
+    /// 
+    /// Give once for mostly quiet, and twice for completely quiet.
+    /// When printing results, only print results after all runs, or nothing.
+    /// When saving data, only print that data has been saved to file, or nothing.
     #[arg(short, long, action = clap::ArgAction::Count)]
     pub quiet: u8,
 
-    // Print the time elapsed during calulation
+    /// Print the time elapsed during calulation
     #[arg(short, long)]
     pub time_elapsed: bool,
 }
