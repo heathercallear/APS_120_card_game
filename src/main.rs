3a5a4d465bd6f23f1a261f9e509f0b51692e3391
Heather Callear <heather.l.c.code@gmail.com>
2026-07-13 18:09:06 +0100

    refactor: seperate results printing into its own function

--- src/main.rs
+++ src/main.rs
@@ -36,26 +36,30 @@ fn main() {
     for exponent in 2..=MAX_EXPONENT {
         game.play_games(already_ran * 9);
         already_ran *= 10;
-        // convert results array items to "0" if 0, else scientific notation
-        let mut results_proportion_str =
-            String::from_iter(game.get_results_proportion().iter().map(|f| {
-                if *f == 0f64 {
-                    format!("{f}, ")
-                } else {
-                    format!("{:.5e}, ", f)
-                }
-            }));
-        // removing trailing commma and space
-        results_proportion_str.pop();
-        results_proportion_str.pop();
-        // print output
-        println!(
-            "10^{0} ({1:>2$}) runs: {3}",
-            exponent,
-            game.total_runs,
-            MAX_EXPONENT + 1,
-            results_proportion_str,
-        );
-        println!();
+        print_game_results(&game, &exponent);
     }
+}
+
+fn print_game_results(game: &Game, exponent: &usize) {
+    // convert results array items to "0" if 0, else scientific notation
+    let mut results_proportion_str =
+        String::from_iter(game.get_results_proportion().iter().map(|f| {
+            if *f == 0f64 {
+                format!("{f}, ")
+            } else {
+                format!("{:.5e}, ", f)
+            }
+        }));
+    // removing trailing commma and space
+    results_proportion_str.pop();
+    results_proportion_str.pop();
+    // print output
+    println!(
+        "10^{0} ({1:>2$}) runs: {3}",
+        exponent,
+        game.total_runs,
+        MAX_EXPONENT + 1,
+        results_proportion_str,
+    );
+    println!();
 }
