aa3721354cf1b6bd364a9844cbc5afdc691b9f41
Heather Callear <heather.l.c.code@gmail.com>
2026-07-14 15:04:57 +0100

    perf: store cards in deck as an array

--- src/card.rs
+++ src/card.rs
@@ -50,37 +50,49 @@ pub struct Card {
 }
 
 pub struct Deck {
-    full: Vec<Card>,
-    current: Vec<Card>,
+    cards: [Card; 52],
     rng: rand::rngs::ThreadRng,
+    index: usize,
 }
 
 impl Deck {
     pub fn new() -> Self {
-        let mut full = Vec::with_capacity(52);
-        for suit in SUITS {
-            for rank in RANKS {
-                full.push(Card { suit, rank });
-            }
-        }
-        let current = Vec::with_capacity(52);
         let mut deck = Self {
-            full,
-            current,
+            cards: Deck::get_sorted_deck(),
             rng: rand::rng(),
+            index: 52,
         };
         deck.reset();
         deck
     }
 
+    pub fn get_sorted_deck() -> [Card; 52] {
+        let mut cards = [Card {
+            suit: Suit::Clubs,
+            rank: Rank::Ace,
+        }; 52];
+        let mut card_index = 0;
+        for suit in SUITS {
+            for rank in RANKS {
+                cards[card_index] = Card { suit, rank };
+                card_index += 1;
+            }
+        }
+        cards
+    }
+
     pub fn reset(&mut self) {
-        self.current.clear();
-        self.current.extend(&self.full);
-        self.current.shuffle(&mut self.rng);
+        self.index = 52;
+        self.cards.shuffle(&mut self.rng);
     }
 
     pub fn pop(&mut self) -> Option<Card> {
-        self.current.pop()
+        if self.index == 0 {
+            None
+        } else {
+            self.index -= 1;
+            Some(self.cards[self.index])
+        }
     }
 }
 
@@ -88,8 +100,10 @@ impl Deck {
 impl Deck {
     /// Set hand to be the same as a full sorted deck.
     pub fn reset_to_sorted(&mut self) {
-        self.current.clear();
-        self.current.extend(&self.full);
+        let cards = Deck::get_sorted_deck();
+        for i in 0..self.cards.len() {
+            self.cards[i] = cards[i];
+        }
     }
 }
 
@@ -137,58 +151,63 @@ mod tests {
     #[test]
     fn deck_new_test() {
         let deck = Deck::new();
-        assert_eq!(deck.full.len(), 52, "Full deck length should be 52");
-        assert_eq!(
-            deck.current.len(),
-            52,
-            "Current deck length should start as 52"
+        assert_eq!(deck.cards.len(), 52, "Deck length should be 52");
+        assert_ne!(
+            deck.cards,
+            Deck::get_sorted_deck(),
+            "Deck should be shuffled"
         );
-        assert_ne!(deck.full, deck.current, "Current deck should be shuffled");
     }
 
     #[test]
     fn deck_pop() {
         let mut deck = Deck::new();
-        assert_eq!(deck.full.len(), 52, "Full deck length should be 52");
-        assert_eq!(
-            deck.current.len(),
-            52,
-            "Current deck length should start as 52"
+        assert_eq!(deck.cards.len(), 52, "Deck length should be 52");
+        assert!(
+            deck.cards != Deck::get_sorted_deck(),
+            "Current deck should be shuffled"
         );
-        assert!(deck.full != deck.current, "Current deck should be shuffled");
         let mut popped_cards = Vec::with_capacity(52);
         for i in 1..=52 {
-            popped_cards.push(deck.pop());
-            assert_eq!(
-                deck.full.len(),
-                52,
-                "Full deck length should not be changed by pop"
+            popped_cards.push(
+                deck.pop()
+                    .expect("Deck should have 52 cards and so keep popping"),
             );
             assert_eq!(
-                deck.current.len(),
+                deck.cards.len(),
+                52,
+                "Deck length should not be changed by pop"
+            );
+            assert_eq!(
+                deck.index,
                 52 - i,
-                "Current deck length should be {} after popping {} times",
+                "Current deck index should be {} after popping {} times",
                 52 - i,
                 i
             );
         }
         // Double check current deck is empty
         assert_eq!(
-            deck.full.len(),
+            deck.cards.len(),
             52,
-            "Full deck length should not be changed after popping"
+            "Deck length should not be changed after popping"
         );
-        assert_eq!(deck.current.len(), 0, "Current deck length should be 0");
+        assert_eq!(deck.index, 0, "Current deck index should be 0");
+        assert_eq!(
+            deck.pop(),
+            None,
+            "Popping fully dealt deck should return `None`"
+        );
         // Double check that reset works
         deck.reset();
         assert_eq!(
-            deck.full.len(),
+            deck.cards.len(),
             52,
             "Full deck length should still be 52 after resetting"
         );
-        assert_eq!(
-            deck.current.len(),
-            52,
+        assert_ne!(
+            Vec::from(&deck.cards),
+            popped_cards.into_iter().rev().collect::<Vec<Card>>(),
             "Current deck length should be 52 again after resetting"
         );
     }
