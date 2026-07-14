433a791a877d400585b0de0b886cd6c6eab9f66f
Heather Callear <heather.l.c.code@gmail.com>
2026-07-14 02:02:37 +0100

    style: remove trailing spaces on documentation comments with `cargo fmt`

--- src/cli.rs
+++ src/cli.rs
@@ -14,11 +14,11 @@ pub struct Cli {
     pub save_data: bool,
 
     /// Print less information (only show final results / show no result at all)
-    /// 
+    ///
     /// Give once for mostly quiet, and twice for completely quiet.
-    /// 
+    ///
     /// When printing results, only print results after all runs, or nothing.
-    /// 
+    ///
     /// When saving data, only print that data has been saved to file, or nothing.
     #[arg(short, long, action = clap::ArgAction::Count)]
     pub quiet: u8,
